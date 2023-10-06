4f1302e6772aba79e55f252ddae27b5a5993542f
szekelyd69 <113088439+szekelyd69@users.noreply.github.com>
2023-10-06 12:59:50 +0200

    Delete 3-4-ora directory

--- 3-4-ora/feladat_004.py
+++
@@ -1,7 +0,0 @@
-# dk9 104-oldal
-aktualis_ev = 2023
-felhasznalo_kora = input('Hány éves vagy? ')
-print(f"Te most {felhasznalo_kora} éves vagy.")
-felhasznalo_kora = int(felhasznalo_kora)
-szuletesi_ev = aktualis_ev - felhasznalo_kora
-print(f'Te ekkor születtél: {szuletesi_ev}')
--- 3-4-ora/feladat_006.py
+++
@@ -1,6 +0,0 @@
- #dk9 108-109 oldal
-gondolt_szam = 4
-tipp = input('gondoltam egy szamra, tippelj')
-tipp = int(tipp)
-if tipp == gondolt_szam:
-     print(f"eltalatad")
